46612da24b6d1af5e5dee21e7822c1ea7fd46b8a
diojer <diogodinosaurus@gmail.com>
2024-06-09 17:12:26 +0100

    final merge

--- data/tilesets/TilesetFloor.tsx
+++ data/tilesets/TilesetFloor.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
+<<<<<<< HEAD
 <tileset version="1.8" tiledversion="1.8.2" name="TilesetFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+=======
+<tileset version="1.10" tiledversion="1.10.2" name="TilesetFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+>>>>>>> 4f087146439bb43058de8ef0e44ce20a122ef050
  <image source="TilesetFloor.png" width="352" height="417"/>
 </tileset>
