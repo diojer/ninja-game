5648a41d88ad000a2abbe5dbe24507780f26abcb
Mouseberg <rhodamsw@gmail.com>
2024-06-21 16:20:24 +0100

    fixed tileset and map

--- data/tilesets/bath room.tsx
+++ data/tilesets/bath room.tsx
@@ -1,4 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="bath room" tilewidth="16" tileheight="16" tilecount="77" columns="11">
- <image source="bath room.png" width="180" height="112"/>
+<tileset version="1.10" tiledversion="1.10.2" name="bath room" tilewidth="16" tileheight="16" tilecount="84" columns="12">
+ <image source="bath room.png" width="195" height="112"/>
+ <tile id="39">
+  <animation>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="55" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="56" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="57" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
